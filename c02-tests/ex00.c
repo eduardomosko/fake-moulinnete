ba8650cc8c404457b22e45abf32d0f09e59c7ff0
Eduardo Mosko <62731713+eduardomosko@users.noreply.github.com>
2021-04-15 14:00:19 -0300

    checks return of strcpy

--- c02-tests/ex00.c
+++ c02-tests/ex00.c
@@ -19,12 +19,14 @@ int	main(void)
 {
 	char *src;
 	char *dest;
+	char *ret;
 
 	src = calloc(11, sizeof(char));
 	src = "0123456789";
 	dest = calloc(11, sizeof(char));
-	ft_strcpy(dest, src);	
+	ret = ft_strcpy(dest, src);	
 	printf("src: %s\n", src);
 	printf("dest: %s\n", dest);
+	printf("ret: %s\n", ret);
 	return (0);
 }
