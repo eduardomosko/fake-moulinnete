233e6f2f985bc257df95f162404a6ded81e46694
Diógenes Zilli <dogezilli@gmail.com>
2021-04-16 15:09:15 -0300

    More test cases on C07 ex01 and ex02 (#3)
    * add more cases on ex01
    
    * add more cases on ex02
    
    Co-authored-by: dludtke- <dludtke-@student.42sp.org.br>

--- c07-tests/ex01.c
+++ c07-tests/ex01.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ex01.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: dludtke- <dludtke-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 21:23:50 by vgoncalv          #+#    #+#             */
-/*   Updated: 2021/04/09 21:29:04 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2021/04/16 00:21:19 by dludtke-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	*ft_range(int min, int max);
+int		*ft_range(int min, int max);
 
-int	main(void)
+int		main(void)
 {
 	int *res;
 	int	offset;
@@ -29,5 +29,64 @@ int	main(void)
 	printf("\n");
 	if (res[offset] == res[offset - 1])
 		printf(" > KO, max number included in array.\n");
+
+	int *arr1;
+	int *arr2;
+	int *arr3;
+	int *arr4;
+	int *arr5;
+	int *arr6;
+
+	arr1 = ft_range(1, 2);
+	arr2 = ft_range(-1, 2);
+	arr3 = ft_range(-3, -1);
+	arr4 = ft_range(-2, -3);
+	arr5 = ft_range(0, 10);
+	arr6 = ft_range(-2, 0);
+
+	offset = 0;
+	while (offset < 1)
+	{
+		printf("%d ", arr1[offset]);
+		offset++;
+	}
+	printf("| min %d ", 1);
+	printf("| max %d", 2);
+	printf("\n");
+	offset = 0;
+	while (offset < 3)
+	{
+		printf("%d ", arr2[offset]);
+		offset++;
+	}
+	printf("| min %d ", -1);
+	printf("| max %d", 2);
+	printf("\n");
+	offset = 0;
+	while (offset < 2)
+	{
+		printf("%d ", arr3[offset]);
+		offset++;
+	}
+	printf("| min %d ", -3);
+	printf("| max %d", -1);
+	printf("\n");
+	offset = 0;
+	while (offset < 10)
+	{
+		printf("%d ", arr5[offset]);
+		offset++;
+	}
+	printf("| min %d ", 0);
+	printf("| max %d", 10);
+	printf("\n");
+	offset = 0;
+	while (offset < 2)
+	{
+		printf("%d ", arr6[offset]);
+		offset++;
+	}
+	printf("| min %d ", -2);
+	printf("| max %d", 0);
 	return (0);
 }
--- c07-tests/ex02.c
+++ c07-tests/ex02.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ex02.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: dludtke- <dludtke-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 22:04:51 by vgoncalv          #+#    #+#             */
-/*   Updated: 2021/04/14 15:17:29 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2021/04/16 00:22:46 by dludtke-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_ultimate_range(int **range, int min, int max);
+int		ft_ultimate_range(int **range, int min, int max);
 
-int	main(void)
+int		main(void)
 {
 	int res;
 	int offset;
@@ -35,5 +35,75 @@ int	main(void)
 	}
 	printf("\nres: %d", res);
 	printf("\n");
+
+	int *arr1;
+	int *arr2;
+	int *arr3;
+	int *arr4;
+	int *arr5;
+	int *arr6;
+	int size1;
+	int size2;
+	int size3;
+	int size4;
+	int size5;
+	int size6;
+
+	size1 = ft_ultimate_range(&arr1, 1, 2);
+	size2 = ft_ultimate_range(&arr2, -1, 2);
+	size3 = ft_ultimate_range(&arr3, -3, -1);
+	size4 = ft_ultimate_range(&arr4, -2, -3);
+	size5 = ft_ultimate_range(&arr5, 0, 10);
+	size6 = ft_ultimate_range(&arr6, -2, 0);
+
+	offset = 0;
+	while (offset < 1)
+	{
+		printf("%d ", arr1[offset]);
+		offset++;
+	}
+	printf("| min %d ", 1);
+	printf("| max %d ", 2);
+	printf("| size %d", size1);
+	printf("\n");
+	offset = 0;
+	while (offset < 3)
+	{
+		printf("%d ", arr2[offset]);
+		offset++;
+	}
+	printf("| min %d ", -1);
+	printf("| max %d ", 2);
+	printf("| size %d", size2);
+	printf("\n");
+	offset = 0;
+	while (offset < 2)
+	{
+		printf("%d ", arr3[offset]);
+		offset++;
+	}
+	printf("| min %d ", -3);
+	printf("| max %d ", -1);
+	printf("| size %d", size3);
+	printf("\n");
+	offset = 0;
+	while (offset < 10)
+	{
+		printf("%d ", arr5[offset]);
+		offset++;
+	}
+	printf("| min %d ", -2);
+	printf("| max %d ", -3);
+	printf("| size %d", size5);
+	printf("\n");
+	offset = 0;
+	while (offset < 2)
+	{
+		printf("%d ", arr6[offset]);
+		offset++;
+	}
+	printf("| min %d ", -2);
+	printf("| max %d ", 0);
+	printf("| size %d", size6);
 	return (0);
 }
