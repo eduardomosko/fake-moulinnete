027798bb6b3ec6dcd435961a4e7eb7ce31c98c1a
Jonadabe <986504+Jonadabe@users.noreply.github.com>
2022-09-26 21:33:08 +0100

    Update wit ft_range function.
    ft_range add

--- c07-tests/ex01.c
+++ c07-tests/ex01.c
@@ -11,8 +11,28 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int		*ft_range(int min, int max);
+
+int	*ft_range(int min, int max)
+{
+	int	*tab;
+	int	i;
+
+	if (min >= max)
+		return (0);
+	if (!(tab = (int *)malloc(sizeof(int) * (max - min))))
+		return (0);
+	i = 0;
+	while (min < max)
+	{
+		tab[i] = min;
+		i++;
+		min++;
+	}
+	return (tab);
+}
 
 int		main(void)
 {
